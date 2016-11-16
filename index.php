54db0d6c93977985f0b7afa049ca146f49ecb61b
Joe Anzalone <joe@shmit.com>
2016-11-15 22:59:24 -0500

    Add viewport meta tag to make it sort of responsive

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width">
     <title>Random Album Art Generator</title>
     <link rel="stylesheet" href="main.css">
 </head>
